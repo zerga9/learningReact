acda247dcd249667a88abec1a05400f8cb09611e
Gadiza Zerari <gadizaz@hotmail.com>
2018-08-16 14:11:10 +0100

    setState value:X

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
+	// In JavaScript classes, you need to always call super when defining the constructor of a subclass. All React component classes that have a constructor should start it with a super(props) call.
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: null
+		};
+	}
+
 	render() {
 		return (
-			<button className="square" onClick={() => alert("click")}>
-				{this.props.value}
+			<button className="square" onClick={() => this.setState({ value: "X" })}>
+				{this.state.value}
 			</button>
 		);
 	}
